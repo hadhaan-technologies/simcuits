eb748ab611b6b45c8a33cd80ef3aae649d6ce1d9
dhanush K <dhanushkrishna2402@icloud.com>
2026-08-29 19:01:28 +0530

    fix(Auth): Trying to keep the user loggedIn even when the refresh

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -78,13 +78,19 @@ export const login = async (req, res) => {
         expiresIn: "7d",
       },
     );
-
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
+    // res.cookie("refreshToken", refreshToken, {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: "strict",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });
 
     return res.status(200).json({
       accessToken,
@@ -143,10 +149,15 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("refreshToken", {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
-      maxAge: 7 * 24 * 60 * 60 * 1000,
+      secure: true,
+      sameSite: "none",
     });
+    // {res.clearCookie("refreshToken", {
+    //   httpOnly: true,
+    //   secure: process.env.NODE_ENV === "production",
+    //   sameSite: "strict",
+    //   maxAge: 7 * 24 * 60 * 60 * 1000,
+    // });}
     res.status(200).json({ message: "Logged Out successfully" });
   } catch (err) {
     console.error(err);
